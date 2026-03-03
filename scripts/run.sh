06235ea0661c15749b6cf936ef026aa2c2c77555
quang-ngh <quangngcs@gmail.com>
2026-03-03 21:52:24 +0000

    resolve conf

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python -m src.inference \
+CUDA_VISIBLE_DEVICES=0 python -m src.inference \
   --model_weights_path checkpoints_torch/solaris.pt \
   --clip_checkpoint_path checkpoints_torch/clip.pt \
   --vae_checkpoint_path checkpoints_torch/vae.pt \
@@ -8,4 +8,4 @@ CUDA_VISIBLE_DEVICES=1 python -m src.inference \
   --dataset_name both_look_away     \
   --num_frames 257 \
   --eval_num_samples 1 \
-  --offload
+  # --offload \
